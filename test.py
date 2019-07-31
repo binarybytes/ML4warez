f2cf60d857770ab0c9bfaea47dcb883a09fbccd2
binarybytes <31395956+binarybytes@users.noreply.github.com>
2019-07-30 20:01:08 -0400

    Update test.py

--- test.py
+++ test.py
@@ -3,4 +3,15 @@
 # $ pip3 install pefile
 # $ pip3 install pandas
 
-#2-download malware dataset
+#2-download malware dataset + gunzip it to same dir where script lives
+#https://github.com/chihebchebbi/Mastering-Machine-Learning-for-Penetration-Testing/blob/master/Chapter03/MalwareData.csv.gz
+#gunzip MalwareData.csv.gz
+
+#import libraries
+import pandas as pd
+
+#instantiate the csv
+malData = pd.read_csv("MalwareData.csv", sep="|")
+
+legit = malData[0:41323].drop(["legitimate"])
+
